9bc9521ac3ae2d17f146c7fe16e499d981c29209
FrancescoDeCaroCarella <francescodecarocarella@gmail.com>
2023-02-17 13:42:39 +0100

    Feat: add meta

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,13 +20,20 @@ export default function Home() {
           property="description"
           content="Fdcc Advice Generator App"
         />
-        <meta name="url" property="url" content="" />
+        <meta
+          name="url"
+          property="url"
+          content="https://fdcc-advice-generator-app.vercel.app/"
+        />
         <meta
           name="site_name"
           property="site_name"
           content="Fdcc Advice Generator App | An advice generator app realized with React framework Next.js"
         />
-        <meta property="og:url" content="" />
+        <meta
+          property="og:url"
+          content="https://fdcc-advice-generator-app.vercel.app/"
+        />
 
         <meta
           property="og:title"
@@ -37,13 +44,27 @@ export default function Home() {
           content="Request: build out an advice generator app using the Advice Slip API and get it looking as close to the original design as possible. More information on Advice Generator App Challenge on Frontend Mentor."
         />
         <meta property="og:type" content="App" />
-        <meta property="og:image" content="" />
+        <meta
+          property="og:image"
+          content="./public/img/Fdcc-Advice-Generator-App.jpg"
+        />
 
         <link rel="icon" href="/favicon.ico" />
-        <link rel="canonical" href="" />
+        <link
+          rel="canonical"
+          href="https://fdcc-advice-generator-app.vercel.app/"
+        />
         <link rel="icon" type="image/x-icon" href="./public/img/favicon.ico" />
-        <link rel="prefetch" as="image" type="image/jpeg" href=""></link>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
+        <link
+          rel="prefetch"
+          as="image"
+          type="image/jpeg"
+          href="./public/img/Fdcc-Advice-Generator-App.jpg"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        ></link>
       </Head>
       <main className={styles.Main}>
         <AdviceContainer />
